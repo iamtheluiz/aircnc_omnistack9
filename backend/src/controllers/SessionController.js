176aee25dce36f00e9dcc502dd76d1efdbbde0a2
iamtheluiz <iamtheluiz.dev@gmail.com>
2019-10-02 20:01:47 -0300

    Adicionado SessionController e cadastro de usuários

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,21 @@
+/* Controlador da Sessão */
+const User = require('../models/User'); // Importa o modelo do Usuário
+
+// Lista de métodos de um controlador: index, show, store, update, destroy
+module.exports = {
+  async store(req, res) {
+    // Busca o e-mail do usuário
+    const { email } = req.body;
+
+    // Verifica se o usuário já existe
+    let user = await User.findOne({ email });
+
+    // Caso não encontre
+    if (!user) {
+      // Cria um novo usuário (precisa finalizar antes de seguir para a próxima linha)
+      user = await User.create({ email });
+    }
+
+    return res.json(user)
+  }
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,27 +2,14 @@
 
 const express = require('express'); // Importa o express
 
+// Importa os controllers
+const SessionController = require('./controllers/SessionController');
+
 // Define a variável que guardará todas as rotas
 const routes = express.Router();
 
-// Define a rota principal do server
-routes.get('/users', (req, res) => {
-  // Req (Request) => Tem as informações da requisição
-  // Res (Response) => Devolve algo para o usuário
-
-  // req.query => Acessa os query params (para filtros)
-  return res.json({ idade: req.query.idade });
-});
-
-routes.post('/users/', (req, res) => {
-  // req.body => Acessa o corpo da requisição
-  return res.json(req.body);
-});
-
-routes.put('/users/:id', (req, res) => {
-  // req.params => Acessa os route params (Parâmetros de rota, para edição e delete)
-  return res.json({ id: req.params.id });
-});
+// Define as possíveis rotas
+routes.post('/sessions/', SessionController.store);
 
 // Exporta as rotas
 module.exports = routes;
