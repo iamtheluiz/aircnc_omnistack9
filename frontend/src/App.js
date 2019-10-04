937798db3d118aa4fb78ef4e17b99edf0022a9c5
iamtheluiz <iamtheluiz.dev@gmail.com>
2019-10-04 11:31:46 -0300

    Adicionada captura de dados do usuário

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api';
 
 // Styles
 import './App.css';
@@ -6,6 +7,22 @@ import logo from './assets/logo.svg';
 
 // Component
 function App() {
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event) {
+    // Não deixa o formulário ser enviado
+    event.preventDefault();
+
+    // Verifica o e-mail
+    const response = await api.post('/sessions', { email });
+
+    // Captura o id do usuário
+    const { _id } = response.data;
+
+    // Armazena o código do usuário
+    localStorage.setItem('@aircnc/user', _id);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnC Logo" />
@@ -15,12 +32,14 @@ function App() {
           Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL *</label>
           <input
             id="email"
             type="email"
             placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
           />
 
           <button className="btn" type="submit">Entrar</button>
