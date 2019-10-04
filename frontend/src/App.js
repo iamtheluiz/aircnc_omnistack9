4e5252ab52257e7f0cf14f3d1f0396a1a57b5770
iamtheluiz <iamtheluiz.dev@gmail.com>
2019-10-04 14:17:52 -0300

    Adicionadas rotas ao sistema

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,20 @@
-import React, { useState } from 'react';
-import api from './services/api';
+import React from 'react';
 
 // Styles
 import './App.css';
 import logo from './assets/logo.svg';
 
+// Rotas
+import Routes from './routes';
+
 // Component
 function App() {
-  const [email, setEmail] = useState('');
-
-  async function handleSubmit(event) {
-    // Não deixa o formulário ser enviado
-    event.preventDefault();
-
-    // Verifica o e-mail
-    const response = await api.post('/sessions', { email });
-
-    // Captura o id do usuário
-    const { _id } = response.data;
-
-    // Armazena o código do usuário
-    localStorage.setItem('@aircnc/user', _id);
-  }
-
   return (
     <div className="container">
-      <img src={logo} alt="AirCnC Logo" />
+      <img src={logo} alt="AirCnC" />
 
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
-        </p>
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-MAIL *</label>
-          <input
-            id="email"
-            type="email"
-            placeholder="Seu melhor e-mail"
-            value={email}
-            onChange={event => setEmail(event.target.value)}
-          />
-
-          <button className="btn" type="submit">Entrar</button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import api from '../../services/api';
+
+export default function Login({ history }) {
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event) {
+    // Não deixa o formulário ser enviado
+    event.preventDefault();
+
+    // Verifica o e-mail
+    const response = await api.post('/sessions', { email });
+
+    // Captura o id do usuário
+    const { _id } = response.data;
+
+    // Armazena o código do usuário
+    localStorage.setItem('@aircnc/user', _id);
+
+    history.push('/dashboard');
+  }
+  
+  return (
+    <>
+      <p>
+        Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+        </p>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">E-MAIL *</label>
+        <input
+          id="email"
+          type="email"
+          placeholder="Seu melhor e-mail"
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+        />
+
+        <button className="btn" type="submit">Entrar</button>
+      </form>
+    </>
+  );
+}
